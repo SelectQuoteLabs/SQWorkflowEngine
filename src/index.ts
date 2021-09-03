fcb39a1e9ea63847f2fa03743dec922607a7875f
Cody Rose <cody.rose@selectquote.com>
2021-09-02 20:40:36 -0500

    add models to export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import workflowMachine from './machines/workflow';
+import { workflowModel } from 'machines/workflow/workflow.machine';
 import stepMachine from './machines/step';
+import { stepModel } from 'machines/step/step.machine';
 import questionMachine from './machines/question';
+import { questionModel } from 'machines/question/question.machine';
 import { textStepMachine } from './machines/textStep.machine';
+import { statusMachine, statusModel } from './machines/status.machine';
 
-export { workflowMachine, stepMachine, questionMachine, textStepMachine };
+export {
+  workflowMachine,
+  workflowModel,
+  stepMachine,
+  stepModel,
+  questionMachine,
+  questionModel,
+  textStepMachine,
+  statusMachine,
+  statusModel,
+};
